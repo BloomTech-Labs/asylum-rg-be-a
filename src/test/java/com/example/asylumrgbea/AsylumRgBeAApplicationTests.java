e23074e2859d0de45d7a61a27a3f9b05aeae6e2f
ashtilawat23 <ashtilawat23@gmail.com>
2022-06-05 23:16:48 -0400

    init project setup

---
+++ src/test/java/com/example/asylumrgbea/AsylumRgBeAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asylumrgbea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsylumRgBeAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
