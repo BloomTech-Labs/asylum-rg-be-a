b0c84eddcf3e7fd3f5b5960ebf038e962241308d
Ronald Walker <ronwalker@sent.com>
2022-06-16 11:43:59 -0700

    create the repository layer

--- src/main/java/com/bloomtech/asylumrgbea/entities/AsylumCase.java
+++ src/main/java/com/bloomtech/asylumrgbea/entities/AsylumCase.java
@@ -1,4 +1,22 @@
 package com.bloomtech.asylumrgbea.entities;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@DynamoDBTable(tableName = "AsylumCase")
+@Data
+@NoArgsConstructor
 public class AsylumCase {
+    @Getter(onMethod_ = {@DynamoDBHashKey})
+    private Integer id;
+    private String asylumOffice;
+    private String citizenship;
+    private String raceOrEthnicity;
+    private String caseOutcome;
+    private String completion;
+    private String currentDate;
 }
--- src/main/java/com/bloomtech/asylumrgbea/repositories/AsylumCaseRepository.java
+++ src/main/java/com/bloomtech/asylumrgbea/repositories/AsylumCaseRepository.java
@@ -1,4 +1,9 @@
 package com.bloomtech.asylumrgbea.repositories;
 
-public interface AsylumCaseRepository {
+import com.bloomtech.asylumrgbea.entities.AsylumCase;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AsylumCaseRepository extends PagingAndSortingRepository<AsylumCase, Integer> {
 }
