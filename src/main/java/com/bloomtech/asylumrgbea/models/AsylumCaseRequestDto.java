f5cf9facbae512d49f833421da1ea7e689b8ae52
uma sitaraman <umamaheshwari.sitaraman@gmail.com>
2022-06-16 14:58:39 -0400

    Created the POJO class using Lombok for AsylumCaseRequestDto.java

--- src/main/java/com/bloomtech/asylumrgbea/models/AsylumCaseRequestDto.java
+++ src/main/java/com/bloomtech/asylumrgbea/models/AsylumCaseRequestDto.java
@@ -1,4 +1,10 @@
 package com.bloomtech.asylumrgbea.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class AsylumCaseRequestDto {
+    private Integer id;
 }
