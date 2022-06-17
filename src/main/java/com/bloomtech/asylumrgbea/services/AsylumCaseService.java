07c9021fd9aab091efe9af3d7094c9faa91e6a69
Kyle Kovalchick <Kovalchickzz@gmail.com>
2022-06-16 20:38:17 -0500

    Added AsylumCaseServiceImpl Implementation
    Added AsylumCaseService Interface.

--- src/main/java/com/bloomtech/asylumrgbea/services/AsylumCaseService.java
+++ src/main/java/com/bloomtech/asylumrgbea/services/AsylumCaseService.java
@@ -1,4 +1,8 @@
 package com.bloomtech.asylumrgbea.services;
 
+import com.bloomtech.asylumrgbea.models.AsylumCaseResponseDto;
+import org.springframework.http.ResponseEntity;
+
 public interface AsylumCaseService {
+	ResponseEntity<Iterable<AsylumCaseResponseDto>> getAllAsylumCases();
 }
--- src/main/java/com/bloomtech/asylumrgbea/services/impl/AsylumCaseServiceImpl.java
+++ src/main/java/com/bloomtech/asylumrgbea/services/impl/AsylumCaseServiceImpl.java
@@ -1,4 +1,23 @@
 package com.bloomtech.asylumrgbea.services.impl;
 
-public class AsylumCaseServiceImpl {
+import com.bloomtech.asylumrgbea.mappers.AsylumCaseMapper;
+import com.bloomtech.asylumrgbea.models.AsylumCaseResponseDto;
+import com.bloomtech.asylumrgbea.repositories.AsylumCaseRepository;
+import com.bloomtech.asylumrgbea.services.AsylumCaseService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AsylumCaseServiceImpl implements AsylumCaseService {
+
+	private AsylumCaseRepository asylumCaseRepository;
+	private AsylumCaseMapper asylumCaseMapper;
+
+	@Override
+	public ResponseEntity<Iterable<AsylumCaseResponseDto>> getAllAsylumCases() {
+		return new ResponseEntity<>(asylumCaseMapper.entitiesToResponseDto(asylumCaseRepository.findAll()), HttpStatus.OK);
+	}
 }
