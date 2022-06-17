0844719545de4286638243dbbf9d53ea9660cd82
uma sitaraman <umamaheshwari.sitaraman@gmail.com>
2022-06-17 15:58:46 -0400

    added the code for ErrorDto.java

--- src/main/java/com/bloomtech/asylumrgbea/models/ErrorDto.java
+++ src/main/java/com/bloomtech/asylumrgbea/models/ErrorDto.java
@@ -1,4 +1,12 @@
 package com.bloomtech.asylumrgbea.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class ErrorDto {
+    private String message;
 }
