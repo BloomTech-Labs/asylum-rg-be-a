23265feadfb17603a2a1695c832825f288c554b7
Jayda Wade <jaydawade@Jaydas-MacBook-Air.local>
2022-06-17 09:59:42 -0400

    Added properties to asylum case response DTO with lombok annotations

--- src/main/java/com/bloomtech/asylumrgbea/models/AsylumCaseResponseDto.java
+++ src/main/java/com/bloomtech/asylumrgbea/models/AsylumCaseResponseDto.java
@@ -1,4 +1,17 @@
 package com.bloomtech.asylumrgbea.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AsylumCaseResponseDto {
+    private String asylumOffice;
+    private String citizenship;
+    private String raceOrEthnicity;
+    private String caseOutcome;
+    private String completion;
+    private String currentDate;
 }
