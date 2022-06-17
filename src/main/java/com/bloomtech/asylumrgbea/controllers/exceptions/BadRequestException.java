0f130f196292d77df4f912e251c9e57334d20f41
Jacob T <Jacob.m.thompson4@gmail.com>
2022-06-17 08:55:18 -0600

    Diagram updated for exception handling and class created

---
+++ src/main/java/com/bloomtech/asylumrgbea/controllers/exceptions/BadRequestException.java
@@ -0,0 +1,4 @@
+package com.bloomtech.asylumrgbea.controllers.exceptions;
+
+public class BadRequestException {
+}
---
+++ src/main/java/com/bloomtech/asylumrgbea/models/ErrorDto.java
@@ -0,0 +1,4 @@
+package com.bloomtech.asylumrgbea.models;
+
+public class ErrorDto {
+}
