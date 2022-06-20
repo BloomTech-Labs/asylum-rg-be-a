bc0fe496af6d10a2a94de1eace7135bda2a95749
Jacob T <Jacob.m.thompson4@gmail.com>
2022-06-20 15:50:08 -0600

    Added validator methods for api calls

--- src/main/java/com/bloomtech/asylumrgbea/controllers/exceptions/AsylumCaseNotFoundException.java
+++ src/main/java/com/bloomtech/asylumrgbea/controllers/exceptions/AsylumCaseNotFoundException.java
@@ -1,11 +1,11 @@
 package com.bloomtech.asylumrgbea.controllers.exceptions;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class AsylumCaseNotFoundException extends RuntimeException{
 
     private  final static long serialVersionUID = 146277653713708386L;
--- src/main/java/com/bloomtech/asylumrgbea/controllers/exceptions/BadRequestException.java
+++ src/main/java/com/bloomtech/asylumrgbea/controllers/exceptions/BadRequestException.java
@@ -1,11 +1,11 @@
 package com.bloomtech.asylumrgbea.controllers.exceptions;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class BadRequestException extends RuntimeException{
 
     private final static long serialVersionUID = 4529555431977284783L;
