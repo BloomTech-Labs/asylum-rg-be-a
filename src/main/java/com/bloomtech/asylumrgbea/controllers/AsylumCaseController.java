ba33958deba4f96a32f086e98e9e5cb1d83c4377
Jayda Wade <jaydawade@Jaydas-MacBook-Air.local>
2022-06-22 13:43:35 -0400

    Updated AsylumCaseController to accept request for a page of elements

--- src/main/java/com/bloomtech/asylumrgbea/controllers/AsylumCaseController.java
+++ src/main/java/com/bloomtech/asylumrgbea/controllers/AsylumCaseController.java
@@ -1,8 +1,10 @@
 package com.bloomtech.asylumrgbea.controllers;
 
+import com.bloomtech.asylumrgbea.models.AsylumCaseRequestDto;
 import com.bloomtech.asylumrgbea.models.AsylumCaseResponseDto;
 import com.bloomtech.asylumrgbea.services.AsylumCaseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,4 +27,7 @@ public class AsylumCaseController {
     public Iterable<AsylumCaseResponseDto> getAllCases() {
         return asylumCaseService.getAllAsylumCases();
     }
+
+    @GetMapping("/{page}")
+    public Iterable<AsylumCaseResponseDto> getAllCases(@RequestParam("page")Integer page, AsylumCaseRequestDto asylumCaseRequestDto) { return asylumCaseService.getAllAsylumCases(asylumCaseRequestDto); }
 }
