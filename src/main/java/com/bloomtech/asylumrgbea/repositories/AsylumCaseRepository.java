7e04d04ddab19a15060228b3e5fb7b71a8ba3898
Deathcon6 <81937679+Deathcon6@users.noreply.github.com>
2022-07-21 16:34:32 -0500

    BL-583 UPDATE AsylumCaseRepository Build a concrete repository class (DAO) UPDATE AsylumCaseServiceImpl to reflect change to concrete AsylumCaseRepository UPDATE AsylumCaseServiceImplTest.java commented out code temporarily

--- src/main/java/com/bloomtech/asylumrgbea/repositories/AsylumCaseRepository.java
+++ src/main/java/com/bloomtech/asylumrgbea/repositories/AsylumCaseRepository.java
@@ -1,15 +1,25 @@
 package com.bloomtech.asylumrgbea.repositories;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.bloomtech.asylumrgbea.entities.AsylumCase;
-import org.socialsignin.spring.data.dynamodb.repository.DynamoDBPagingAndSortingRepository;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScanCount;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-@EnableScan
-@EnableScanCount
-public interface AsylumCaseRepository extends DynamoDBPagingAndSortingRepository<AsylumCase, Integer> {
+@RequiredArgsConstructor
+public class AsylumCaseRepository {
+
+    @Autowired
+    private final DynamoDBMapper dynamoDBMapper;
+
+    //add methods as if this were the DAO
+    public Iterable<AsylumCase> findAll() {
+        return dynamoDBMapper.scan(AsylumCase.class, new DynamoDBScanExpression());
+    }
+
+    public void saveAll(Iterable<AsylumCase> cases) {
+        dynamoDBMapper.batchSave(cases);
+    }
 }
